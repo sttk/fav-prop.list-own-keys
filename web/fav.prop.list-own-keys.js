a660062edbb68e2d9451ff83b7cbfca7461d5e06
sttk <sttk.xslet@gmail.com>
2017-11-29 07:35:29 +0900

    Modify a variable name and add a comment

--- web/fav.prop.list-own-keys.js
+++ web/fav.prop.list-own-keys.js
@@ -13,14 +13,14 @@ function listOwnKeys(obj) {
     case 'function': {
       var arr = Object.getOwnPropertyNames(obj);
       for (var i = arr.length - 1; i >= 0; i--) {
-        var elm = arr[i];
+        var key = arr[i];
         /* istanbul ignore if */
-        if (elm === 'caller' || elm === 'arguments') {
+        if (key === 'caller' || key === 'arguments') {
           arr.splice(i, 1);
         }
       }
       /* istanbul ignore if */
-      if (!('name' in obj)) {
+      if (!('name' in obj)) { // A function don't have `name` prop on IE
         arr.push('name');
       }
       return arr;
